9b7f78071eb226a549b55b6f121eeed5207c592c
Dan <digaleev@gmail.com>
2011-07-05 21:44:45 +0600

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRails::Application.config.secret_token = '93b3258249120c4c3ee7d2da04d1b8b1fef6c429e8eb349f0ed6671a33f11c7e982de1dfc895474b674badfe3c149f70e24f67800cbb7f7dc68dcafa5a3e8051'
